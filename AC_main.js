fa74b2ecae703038797186d86def8132d1e3c0d4
A12345678910raghuvanshi12345678910 <97298557+A12345678910raghuvanshi12345678910@users.noreply.github.com>
2023-05-22 18:57:54 +0530

    Add files via upload

---
+++ AC_main.js
@@ -0,0 +1,31 @@
+img ="";
+status_js="";
+function setup() { 
+    canvas = createCanvas(640, 420);
+    canvas.center();    
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = " status: detecting objects";
+}
+function preload() {
+    img = loadImage("AC.jpg");
+}
+function draw(){
+    image(img, 0, 0, 640, 420 );
+    fill("#000000");
+    text("AC", 45, 75);
+    noFill();
+    stroke("#000000");
+    rect(30,60,450,350);
+
+}
+function modelLoaded() {
+    console.log("model is loaded by Aarohi");
+    status_js = true;
+    ObjectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+    if(error)  {
+        console.log(error);
+    }
+    console.log(results);
+}
